6e3147e78085e5f6aa6b78980568343561d4a831
Mahdi <135426196+Mahdi-500@users.noreply.github.com>
2024-04-27 21:08:07 +0330

    added a function to handle pandas exception

--- error.py
+++ error.py
@@ -1,6 +1,6 @@
 import time
 import os
-
+import pandas as pd
 
 def length(number, length) -> bool:
     flag = False
@@ -9,40 +9,28 @@ def length(number, length) -> bool:
     return flag
 
 
-# check's if it's string
-
-
-def just_str(x) -> bool:
+def just_str(x) -> bool:  # todo - check's if it's string
     flag = False
     if str(x).isalpha() == False:
         flag = True
     return flag
 
 
-# check's if it's number
-
-
-def just_number(id) -> bool:
+def just_number(id) -> bool:  # todo - check's if it's number
     flag = False
     if str(id).isdigit() == False:
         flag = True
     return flag
 
 
-# check's if it's just string and number
-
-
-def str_int(x) -> bool:
+def str_int(x) -> bool:  # todo - check's if it's just string and number
     flag = False
     if str(x).isalnum() == False:
         flag = True
     return flag
 
 
-# exception handling
-
-
-def try_except(var_name, text) -> bool and str or int:
+def try_except(var_name, text) -> bool and str or int:  # todo - exception handling
     flag = False
     try:
         var_name = input(text)
@@ -53,7 +41,17 @@ def try_except(var_name, text) -> bool and str or int:
         return flag, var_name
 
 
+def try_except_pd(path) -> bool:  # todo - exception handeling for pandas
+    flag = False
+    
+    try:
+        pd.read_csv(path)
+    except pd.errors.EmptyDataError or FileNotFoundError:
+        flag = True
+
+    return flag
+
 def message(text) -> None:
     print("\n" + text)
-    time.sleep(3)
+    time.sleep(5)
     os.system("cls")
